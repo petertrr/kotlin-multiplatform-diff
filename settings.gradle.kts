d76cbdec1322aaa170289990e6c5449b34771a27
Peter Trifanov <peter.trifanov@mail.ru>
2022-11-26 22:52:06 +0600

    Add `gradle-enterprise` plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,20 @@
+plugins {
+    id("com.gradle.enterprise") version("3.11.4")
+}
+
 rootProject.name = "kotlin-multiplatform-diff"
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+    }
+}
+
+if (System.getenv("CI") != null) {
+    gradleEnterprise {
+        buildScan {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
     }
 }
