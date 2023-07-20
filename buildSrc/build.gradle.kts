3097462fffd6e16fd7574aca5e42ae1cd7e7ba12
Peter Trifanov <peter.trifanov@gmail.com>
2023-07-20 18:29:17 +0000

    reckon settings plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,6 @@ repositories {
 
 val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 dependencies {
-    implementation("org.ajoberstar.reckon:reckon-gradle:0.18.0")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.findVersion("kotlin").get()}")
